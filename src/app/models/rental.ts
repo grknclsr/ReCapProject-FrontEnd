e6ddeacd6ada122cdc622759a44d396567b0ce45
GURKAN CALISIR <gurkan01c@gmail.com>
2021-03-13 19:00:46 +0300

    Updated!

---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental{
+    rentalId:number;
+    brandName:string;
+    colorName:string;
+    userName:string;
+    returnDate:number;
+    rentDate:number;
+}
---
+++ src/app/services/colorService.spec.ts
@@ -0,0 +1,17 @@
+
+import { TestBed } from '@angular/core/testing';
+
+import { ColorService } from './colorService';
+
+describe('ColorService', () => {
+  let service: ColorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ColorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
